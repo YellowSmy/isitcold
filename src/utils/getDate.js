abdcab0888a1b2c841ce78530b728fe914c51ac8
YellowSmy <fprh2005@naver.com>
2025-03-08 15:43:00 +0900

    Bugfix: future date load fix

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,32 +1,37 @@
 //Date Function
-export function getDate(updateTime) {
+export function getDate(updateTimes) {
     const today = new Date();
-    const current_time = (today.getHours()*100) + today.getMinutes();
+    //const current_time = (today.getHours()*100) + today.getMinutes();
+    const current_time = 100;
     
     let base_date, last_base_year, base_time;
   
-    if(updateTime != undefined) {
+    if(updateTimes != undefined) {
+      //block future time
+      const pastTimes = updateTimes.filter(time => (time <= current_time));
+
       //update time
-      base_time =  updateTime.reduce((update_time, time) => { // 누적값, 현재값
+      base_time =  pastTimes.reduce((update_time, time) => { // 누적값, 현재값
         return (Math.abs(update_time-current_time) < Math.abs(time-current_time) ? update_time : time);
-      }, updateTime[0]);
+      }, updateTimes[0]);
   
       // month & year change setting
-      if (current_time < updateTime[0]) {
+      if (current_time < updateTimes[0]) {
         const yesterday = new Date(today);
         yesterday.setDate(yesterday.getDate()-1);
       
         base_date = yesterday.getFullYear().toString()
                   + (yesterday.getMonth()+1).toString().padStart(2, "0") 
                   + yesterday.getDate().toString().padStart(2, "0");
-        base_time = updateTime[updateTime.length - 1];
+        base_time = updateTimes[updateTimes.length - 1];
       }
-  
-      //String invert for API protocol
-      base_date = today.getFullYear().toString() 
-                      + (today.getMonth()+1).toString().padStart(2, "0") 
-                      + today.getDate().toString().padStart(2, "0");
-      base_time = base_time.toString().padStart(4, '0');
+      else {
+        //String invert for API protocol
+        base_date = today.getFullYear().toString() 
+        + (today.getMonth()+1).toString().padStart(2, "0") 
+        + today.getDate().toString().padStart(2, "0");
+        base_time = base_time.toString().padStart(4, '0');
+      }
     }
   
     //last year for ASOS information
@@ -37,4 +42,4 @@ export function getDate(updateTime) {
                     + last_year.getDate().toString().padStart(2, "0");
   
     return {"base_date":base_date, "base_time": base_time, "last_base_year": last_base_year};
-  }
+}
